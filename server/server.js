51d8345e65d043a1acf6dd8cb28c38817bd231fc
kaye <kevinyusyus@gmail.com>
2021-03-19 10:05:27 +0700

    add package.json script

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const http = new Server(app);
 
 socket(http);
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(cors());
 app.use(express.json());
 app.use(require('morgan')('common'));
@@ -18,7 +18,7 @@ app.use(require('morgan')('common'));
 //   res.sendFile(__dirname + '/index.html');
 // });
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 app.use('/api', require('./routes'));
 
